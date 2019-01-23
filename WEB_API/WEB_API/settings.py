81493c07985fa9cc52b951cf97dbfc8398fef549
aafsari <amirafsari99@yahoo.com>
2019-01-23 15:43:09 +0330

    env

--- WEB_API/WEB_API/settings.py
+++ WEB_API/WEB_API/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = '&q(&9vy12r*3v1g4s-99pz=j@ws1e%s0j2mu&7uidmqlbwxaj3'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -41,6 +39,8 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'authentication.accounts',
     'authentication.teams',
+    'corsheaders',
+
 ]
 
 MIDDLEWARE = [
@@ -51,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
+
 ]
 
 ROOT_URLCONF = 'WEB_API.urls'
@@ -73,7 +75,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'WEB_API.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
@@ -83,7 +84,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -103,7 +103,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
@@ -116,7 +115,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
@@ -133,6 +131,9 @@ EMAIL_PORT = 587
 VERIFICATION_KEY_EXPIRY_DAYS = 2
 
 SITE_NAME = "WEB API"
+
+CORS_ORIGIN_WHITELIST = 'localhost:3000'
+CORS_ORIGIN_ALLOW_ALL = False
 
 try:
     from local_settings import *
