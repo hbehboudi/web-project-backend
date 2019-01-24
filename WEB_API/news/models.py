cae11913b559b7567cd70a0d89359bdaacbaf92d
Hossein <hbehboudi@ce.sharif.edu>
2019-01-24 10:41:52 +0330

    updated.

--- WEB_API/news/models.py
+++ WEB_API/news/models.py
@@ -3,13 +3,6 @@ import uuid
 from django.db import models
 
 
-class Tag(models.Model):
-    tag = models.CharField(primary_key=True, max_length=32)
-
-    def __str__(self):
-        return self.tag
-
-
 class News(models.Model):
     FIELDS = (
         ('FTB', 'Football'),
@@ -17,20 +10,20 @@ class News(models.Model):
         ('OTH', 'Other'),
     )
 
-    title = models.CharField(max_length=128)
-    summary = models.CharField(max_length=256, null=True, blank=True)
-    text = models.TextField(null=True, blank=True)
-    category = models.CharField(max_length=16, null=True)
-    created_date_time = models.DateTimeField()
-    image_url = models.URLField(null=False, blank=True)
-    field = models.CharField(max_length=3, choices=FIELDS, default='OTH')
+    title = models.CharField(max_length=128, verbose_name='عنوان')
+    summary = models.CharField(max_length=256, null=True, blank=True, verbose_name='خلاصه خبر')
+    text = models.TextField(null=True, blank=True, verbose_name='متن خبر')
+    category = models.CharField(max_length=16, null=True, verbose_name='نوع خبر')
+    created_date_time = models.DateTimeField(verbose_name='زمان ساخت')
+    image_url = models.URLField(null=False, blank=True, verbose_name='آدرس تصویر')
+    field = models.CharField(max_length=3, choices=FIELDS, default='OTH', verbose_name='ورزش')
     url = models.UUIDField(default=uuid.uuid4, db_index=True, unique=True, editable=False, auto_created=True)
-    tags = models.ManyToManyField(Tag, blank=True)
-    deleted = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, blank=True, verbose_name='تگ ها')
+    deleted = models.BooleanField(default=False, verbose_name='حذف شده')
 
     def __str__(self):
         return self.title
 
     class Meta:
-        verbose_name = 'News'
+        verbose_name = 'خبر'
         ordering = ('-created_date_time', 'title')
