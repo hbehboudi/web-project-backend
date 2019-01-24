aa0ee5581146c621990e08704945d0eb49611762
Hossein <hbehboudi@ce.sharif.edu>
2019-01-24 15:34:16 +0330

    TeamInLeague app created.

--- WEB_API/WEB_API/settings.py
+++ WEB_API/WEB_API/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'team',
     'tag',
     'player',
+    'teamInLeague',
 ]
 
 MIDDLEWARE = [
---
+++ WEB_API/teamInLeague/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+from teamInLeague.models import TeamLeague
+
+admin.site.register(TeamLeague)
