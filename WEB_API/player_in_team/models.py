f087fa79a02245f1ee3d795309d0e704c96d5376
Hossein <hbehboudi@ce.sharif.edu>
2019-01-26 12:54:14 +0330

    updated.

--- WEB_API/player_in_team/models.py
+++ WEB_API/player_in_team/models.py
@@ -6,9 +6,12 @@ from team.models import Team
 from team_in_league.models import TeamLeague
 
 
+
+
 class PlayerTeam(models.Model):
     player = models.ForeignKey(Player, on_delete=True, verbose_name='بازیکن')
     teamLeague = models.ForeignKey(TeamLeague, on_delete=True, verbose_name='تیم در لیگ')
+    num = models.IntegerField(verbose_name='شماره پیراهن')
 
     created_date_time = models.DateTimeField(verbose_name='زمان ساخت')
     deleted = models.BooleanField(default=False, verbose_name='حذف شده')
