00d8eff2600e25fd51a0bff2e09f084f364ca248
Hossein <hbehboudi@ce.sharif.edu>
2019-02-01 12:06:24 +0330

    Team api updated.

--- WEB_API/team/views.py
+++ WEB_API/team/views.py
@@ -59,7 +59,7 @@ def info(request, team_slug):
     try:
         team = Team.objects.filter(slug__contains=team_slug, deleted=False)
         team_info = team.values('name', 'image_url', 'nickname', 'internationalRank', 'city', 'country',
-                                'establishedYear', 'coach', 'captain', 'website')
+                                'establishedYear', 'coach', 'captain', 'website', 'field')
         return Response(team_info[0])
     except (IndexError, AssertionError, OperationalError):
         return Response({})
@@ -82,16 +82,11 @@ def team_statistics(request, team_slug):
         team = Team.objects.filter(slug__contains=team_slug, deleted=False)[0]
         leagues = TeamLeague.objects.filter(team=team, league__active=False, deleted=False).values('league__name',
                                                                                                    'league__year',
-                                                                                                   'result')
+                                                                                                   'result',
+                                                                                                   'league__field')
         for league in leagues:
             league['game_number'] = Game.objects.filter(league__name=league['league__name'], deleted=False). \
                 filter(Q(team1=team) | Q(team2=team)).count()
-
-            league['scoring_goal_number'] = Goal.objects. \
-                filter(game__league__name=league['league__name'], deleted=False, scoring_team=team).count()
-
-            league['receiving_goal_number'] = Goal.objects. \
-                filter(game__league__name=league['league__name'], deleted=False, receiving_team=team).count()
 
             league['win_num'] = Game.objects.filter(league__name=league['league__name'], deleted=False). \
                 filter(Q(team1=team, team_state1='W') | Q(team2=team, team_state2='W')).count()
@@ -99,8 +94,15 @@ def team_statistics(request, team_slug):
             league['lose_num'] = Game.objects.filter(league__name=league['league__name'], deleted=False). \
                 filter(Q(team1=team, team_state1='L') | Q(team2=team, team_state2='L')).count()
 
-            league['draw_num'] = Game.objects.filter(league__name=league['league__name'], deleted=False). \
-                filter(Q(team1=team, team_state1='D') | Q(team2=team, team_state2='D')).count()
+            if league['league__field'] == 'FTB':
+                league['scoring_goal_number'] = Goal.objects. \
+                    filter(game__league__name=league['league__name'], deleted=False, scoring_team=team).count()
+
+                league['receiving_goal_number'] = Goal.objects. \
+                    filter(game__league__name=league['league__name'], deleted=False, receiving_team=team).count()
+
+                league['draw_num'] = Game.objects.filter(league__name=league['league__name'], deleted=False). \
+                    filter(Q(team1=team, team_state1='D') | Q(team2=team, team_state2='D')).count()
 
         return Response(leagues)
     except IndexError:
@@ -116,7 +118,8 @@ def league_list(request, team_slug):
         for league in leagues:
             league['teams'] = TeamLeague.objects. \
                 filter(league__name=league['league__name'], league__year=league['league__year']).values('team__name',
-                                                                                                        'team__slug')
+                                                                                                        'team__slug',
+                                                                                                        'team__field')
             for team in league['teams']:
                 team['game_number'] = Game.objects.filter(deleted=False, league__name=league['league__name'],
                                                           league__year=league['league__year']). \
@@ -131,16 +134,16 @@ def league_list(request, team_slug):
                                                           league__year=league['league__year']). \
                     filter(Q(team1__name=team['team__name'], team_state1='L') |
                            Q(team2__name=team['team__name'], team_state2='L')).count()
+                if team['team__field'] == "FTB":
+                    team['draw_number'] = Game.objects.filter(deleted=False, league__name=league['league__name'],
+                                                              league__year=league['league__year']). \
+                        filter(Q(team1__name=team['team__name'], team_state1='D') |
+                               Q(team2__name=team['team__name'], team_state2='D')).count()
 
-                team['draw_number'] = Game.objects.filter(deleted=False, league__name=league['league__name'],
-                                                          league__year=league['league__year']). \
-                    filter(Q(team1__name=team['team__name'], team_state1='D') |
-                           Q(team2__name=team['team__name'], team_state2='D')).count()
-
-                team['score'] = team['win_number'] * 3 + team['draw_number']
+                    team['score'] = team['win_number'] * 3 + team['draw_number']
         return Response(leagues)
     except IndexError:
-        return Response({})
+        return Response({'h': 2})
 
 
 @api_view()
@@ -150,10 +153,16 @@ def game_list(request, team_slug):
         name = request.GET.get('n')
 
         team = Team.objects.filter(slug__contains=team_slug, deleted=False)[0]
-        games = Game.objects.filter(Q(team1=team) | Q(team2=team)).values('team1__name', 'team2__name',
-                                                                          'team1__image_url', 'team2__image_url',
-                                                                          'goals1', 'goals2', 'game_date', 'full_time',
-                                                                          'team1__slug', 'team2__slug', 'slug')
+        if team.field == 'FTB':
+            games = Game.objects.filter(Q(team1=team) | Q(team2=team)).values('team1__name', 'team2__name', 'full_time',
+                                                                              'team1__image_url', 'team2__image_url',
+                                                                              'goals1', 'goals2', 'game_date',
+                                                                              'team1__slug', 'team2__slug', 'slug')
+        else:
+            games = Game.objects.filter(Q(team1=team) | Q(team2=team)).values('team1__name', 'team2__name', 'full_time',
+                                                                              'team1__image_url', 'team2__image_url',
+                                                                              'all_score1', 'all_score2', 'game_date',
+                                                                              'team1__slug', 'team2__slug', 'slug')
         if state:
             games = games.filter(Q(team1=team, team_state1=state) | Q(team2=team, team_state2=state))
         if name:
